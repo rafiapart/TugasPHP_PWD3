aa4a1623dbc751f285fff160a69e53c59fb2892a
rafii <decik733@gmail.com>
2025-11-17 20:05:16 +0700

    NO 1

---
+++ genap.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Genap</title>
+</head>
+<body>
+	<html>Angka yang genap adalah : </html>
+</body>
+</html>
+
+
+
+
+
+
+<?php
+for($i = 1; $i <= 10; $i++){
+    if($i % 2 == 0){		
+        echo $i . " ";
+    }
+}
+?>
